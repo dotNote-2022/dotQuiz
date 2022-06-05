b852e64bd9d8b202978fd189e2d93756aca7d776
simonkarofillidis <skarofyl@csd.auth.gr>
2022-06-05 17:08:42 +0300

    Initial stage for game information

---
+++ app/src/main/java/com/example/dotnote/ui/slideshow/GameInformation.java
@@ -0,0 +1,38 @@
+package com.example.dotnote.ui.slideshow;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dotnote.R;
+
+public class GameInformation extends Fragment {
+
+    private GameInformationViewModel mViewModel;
+
+    public static GameInformation newInstance() {
+        return new GameInformation();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_game_information, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(GameInformationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/dotnote/ui/slideshow/GameInformationViewModel.java
@@ -0,0 +1,7 @@
+package com.example.dotnote.ui.slideshow;
+
+import androidx.lifecycle.ViewModel;
+
+public class GameInformationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
